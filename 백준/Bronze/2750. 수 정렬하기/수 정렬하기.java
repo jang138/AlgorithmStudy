e5a7117c770b77438db4dd7826af50f351dea4c4
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-05-01 18:26:14 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 12532 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,24 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int input[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(input);
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(input[i]);
+		}
+	}
+}
