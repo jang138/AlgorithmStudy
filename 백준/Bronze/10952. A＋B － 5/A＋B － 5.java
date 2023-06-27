88a72870b5d291718177ba71ef30a0f34b35c2e0
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-06-27 18:09:54 +0900

    [Bronze V] Title: A+B - 5, Time: 88 ms, Memory: 11712 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		while (true) {
+			String str = br.readLine();
+			st = new StringTokenizer(str);
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a == 0 && b == 0) {
+				break;
+			}
+
+			System.out.println(a + b);
+
+		}
+
+	}
+
+}
