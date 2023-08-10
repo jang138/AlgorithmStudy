c7b3463a0803714d3f4517aae870c547ab07c013
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-08-11 03:25:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,64 +4,59 @@ import java.util.Arrays;
 
 public class Main {
 
+	static int N, num;
+	static int map[][];
+	static boolean visit[][];
 	static int dx[] = { 1, 0, -1, 0 };
 	static int dy[] = { 0, 1, 0, -1 };
 	static int apart[];
-	static int map[][];
-	static boolean visit[][];
-	static int N;
-	static int num = 0;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		apart = new int[N * N];
 		map = new int[N][N];
 		visit = new boolean[N][N];
+		apart = new int[N * N + 1];
 
 		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
 			for (int j = 0; j < N; j++) {
 				map[i][j] = line.charAt(j) - '0';
-				// System.out.print(map[i][j]);
 			}
-			// System.out.println();
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j] == 1 && !visit[i][j]) {
+				if (!visit[i][j] && map[i][j] == 1) {
 					num++;
 					dfs(i, j);
 				}
 			}
 		}
-
+		
 		System.out.println(num);
 		Arrays.sort(apart);
-
-		for (int i = 0; i < N * N; i++) {
-			if(apart[i] == 0)
+		
+		for (int i = 0; i < apart.length; i++) {
+			if(apart[i]==0)
 				continue;
-			
 			System.out.println(apart[i]);
 		}
-
 	}
 
-	static void dfs(int x, int y) {
+	private static void dfs(int x, int y) {
 		visit[x][y] = true;
-		apart[num]++; // 단지에 해당되는 집 수 카운팅
+		apart[num]++;
 
 		for (int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
 
-			if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-				if (map[nx][ny] == 1 && !visit[nx][ny]) {
-					dfs(nx, ny);
-				}
+			if (nx < 0 || ny < 0 || nx >= N || ny >= N || visit[nx][ny] || map[nx][ny] != 1) {
+				continue;
 			}
+
+			dfs(nx, ny);
 		}
 	}
 
