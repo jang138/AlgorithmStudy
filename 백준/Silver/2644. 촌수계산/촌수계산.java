62e9087080484804a57d34d810ae0ca23bbf8a1b
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-08-21 14:53:29 +0900

    [Silver II] Title: 촌수계산, Time: 76 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,64 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, A, B, M;
-	static int family[][];
 	static int chon[];
-	static Queue<Integer> queue = new LinkedList<>();
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
-		N = Integer.parseInt(st.nextToken());
-		family = new int[N + 1][N + 1];
-		chon = new int[N + 1];
-
+		N = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		A = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
-		
 		M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < M; i++) {
+
+		chon = new int[N + 1];
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			family[x][y] = family[y][x] = 1;
+
+			graph.get(x).add(y);
+			graph.get(y).add(x);
 		}
-		
-		bfs(A);
-				
+
+		dfs(A);
+
 		System.out.println(chon[B] == 0 ? -1 : chon[B]);
-	}
-	
-	private static void bfs(int num) {
-		queue.add(num);
-		
-		while(!queue.isEmpty()) {
-			int target = queue.poll();
-//			System.out.println( "target : " + target);
-			
-			if(target == B) {
-				break;
-			}
-			
-			for (int i = 1; i <= N; i++) {
-				if(family[target][i] == 1 && chon[i] == 0) {
-//					System.out.print(i + " ");
-					queue.add(i);
-					chon[i] = chon[target] + 1;
-				}
-				
-			}
-			
-		}
-		
+
 	}
 
+	private static void dfs(int n) {
+		if (n == B) {
+			return;
+		}
+
+		for (int now : graph.get(n)) {
+			if (chon[now] == 0) {
+				chon[now] = chon[n] + 1;
+				dfs(now);
+			}
+		}
+
+	}
 }
