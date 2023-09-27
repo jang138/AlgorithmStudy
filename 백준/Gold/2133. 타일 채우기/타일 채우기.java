5b691f73d34138cb50d0d1100643acf438276481
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-09-27 16:35:07 +0900

    [Gold IV] Title: 타일 채우기, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		if (N % 2 == 1) {
+			System.out.println(0);
+			return;
+		}
+
+		int dp[] = new int[N + 1];
+		dp[0] = 1;
+		dp[2] = 3;
+
+		// DP[N] = DP[N - 2] * DP[2] + DP[N - 4] * 2 + DP[N - 6] * 2 + DP[N - 8] * 2
+		for (int i = 4; i <= N; i += 2) {
+			dp[i] = dp[i - 2] * dp[2];
+
+			for (int j = i - 4; j >= 0; j -= 2) {
+				dp[i] += (dp[j] * 2);
+			}
+
+		}
+
+		System.out.println(dp[N]);
+
+	}
+
+}
