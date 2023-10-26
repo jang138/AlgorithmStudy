13effaf6601829af3a4fd664b5181677f9c408bd
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-10-26 17:00:29 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,16 +1,19 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		String str = "";
 
 		while ((str = br.readLine()) != null) {
-
-			System.out.println(str);
+			sb.append(str).append("\n");
 		}
+
+		System.out.println(sb);
+
 	}
 
 }
