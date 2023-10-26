b4b5d8639d6b9546a325ea0d749c3515426e9d2a
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-10-26 16:33:48 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 116 ms, Memory: 13712 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int array[][] = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				array[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				sb.append(array[i][j] + Integer.parseInt(st.nextToken())).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
