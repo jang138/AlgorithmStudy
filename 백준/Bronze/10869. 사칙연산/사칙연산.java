f10338ab87b9ecf335489ac3f2dd8e3c2abc6aba
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-10-26 15:04:44 +0900

    [Bronze V] Title: 사칙연산, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		sb.append(A+B).append("\n");
+		sb.append(A-B).append("\n");
+		sb.append(A*B).append("\n");
+		sb.append(A/B).append("\n");
+		sb.append(A%B).append("\n");
 		
-		System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-
+		System.out.println(sb);
 	}
 
 }
