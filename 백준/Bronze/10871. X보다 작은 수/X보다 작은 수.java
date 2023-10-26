0f71a956e73b1de94618df694a0ef9b3f082c2f4
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-10-26 16:18:26 +0900

    [Bronze V] Title: X보다 작은 수, Time: 108 ms, Memory: 13260 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			if (a < X) {
+				sb.append(a).append(" ");
+			}
+		}
+
+		System.out.println(sb);
+	}
+
+}
