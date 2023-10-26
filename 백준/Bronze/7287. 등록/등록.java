01924234ab99b25c0f0f7532bf690983bac8eeaf
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-10-26 14:52:22 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11232 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("32").append("\n");
+		sb.append("toipoodle").append("\n");
+
+		System.out.println(sb);
+	}
+
+}
