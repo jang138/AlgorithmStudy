dcdb60abf6a307320d74da19f5f2fe021bc71695
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-11-02 18:13:27 +0900

    [level 2] Title: 타겟 넘버, Time: 0.37 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    static int count = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        
+        return count;
+    }
+    
+    public void dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.length){
+            if(sum == target){
+                count++;
+            }
+            return;
+        }
+        
+        dfs(depth + 1, sum += numbers[depth], numbers, target);
+        sum -= numbers[depth];
+        
+        dfs(depth + 1, sum -= numbers[depth], numbers, target);
+        sum += numbers[depth];
+    }
+}
