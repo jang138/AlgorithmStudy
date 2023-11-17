ba9ee4efbdfcf42af5a5fbafd2551509b9481f9c
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-11-17 15:54:41 +0900

    [level 2] Title: 전화번호 목록, Time: 217.89 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,24 +1,6 @@
-import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String number : phone_book){
-            map.put(number, 1);
-        }
-        
-        Main:
-        for(String number : phone_book){
-            for(int i = 0; i < number.length(); i++){
-                String str = number.substring(0, i);
-                if(map.containsKey(str)){
-                    answer = false;
-                    break Main;
-                }
-            }
-        }
-        
         return answer;
     }
 }
