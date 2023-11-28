2466511ef9a9116c06ce7770de1bd391a314e3ab
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-11-28 17:45:49 +0900

    [Gold III] Title: 나머지 합, Time: 472 ms, Memory: 168828 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -8,45 +8,48 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-
-		long S[] = new long[N];
-		long C[] = new long[M];
-		long ans = 0;
+		long S[] = new long[N + 1];
+		long remainArray[] = new long[M];
+		long cnt = 0;
 
 		st = new StringTokenizer(br.readLine());
-		S[0] = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < N; i++) {
-			int a = Integer.parseInt(st.nextToken());
-			S[i] = a + S[i - 1];
+		for (int i = 1; i <= N; i++) {
+			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+//		System.out.println(Arrays.toString(S));
+
+		/*
+		 * S[i] % M 과 S[j] % M 의 값이 같다면 (S[i] - S[j]) % M = 0이다. S[i] % M 으로 배열을 바꾸어준다.
+		 * 이때 0이 되는 것은 처음부터 i번째 배열까지 합이 0이라는 의미이다.
+		 */
+
+		for (int i = 1; i <= N; i++) {
+			int remain = (int) (S[i] % M);
+			remainArray[remain]++;
+
+			if (remain == 0)
+				cnt++;
 		}
 
 		/*
-		 * ( S[i] - S[j] ) % M = 0 
-		 * ( S[i] % M ) - ( S[j] % M ) = 0 
-		 * ( S[i] % M ) = ( S[j] % M )
-		 * 누적합의 값이 같다면 나머지가 0이 되는 경우다.
+		 * 각 나머지에 해당하는 개수를 remainArray 배열에 저장한다. 이때, 개수가 2 이상이라면 전체 개수에서 2개를 뽑는 것은 누적합
+		 * 배열에서 i와 j를 선택해 차이를 구하는 것과 같다. nC2 = n * (n-1) / 2
 		 */
 
-		for (int i = 0; i < N; i++) {
-			int remain = (int) (S[i] % M);
-			if (remain == 0) {
-				ans++;
-			}
-
-			C[remain]++;
-		}
-
-		// 조합 nC2 = n * n-1 / 2
-		for (int i = 0; i < M; i++) {
-			if (C[i] > 1) {
-				ans += (C[i] * (C[i] - 1) / 2);
+		for (int remain = 0; remain < M; remain++) {
+			if (remainArray[remain] > 1) {
+				cnt += (remainArray[remain] * (remainArray[remain] - 1) / 2);
 			}
 		}
 
-		System.out.println(ans);
+//		System.out.println(Arrays.toString(remainArray));
+		System.out.println(cnt);
+
 	}
 
 }
