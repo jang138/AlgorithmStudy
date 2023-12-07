d9e597f5fd68183bf011fe2303a76fb9dfc0bc6f
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2023-12-07 22:11:03 +0900

    [Gold IV] Title: 좋다, Time: 136 ms, Memory: 13004 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -8,10 +9,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		if (N <= 2) {
-			System.out.println(0);
-			return;
-		}
+//		if (N <= 2) {
+//			System.out.println(0);
+//			return;
+//		}
 
 		long numbers[] = new long[N];
 
