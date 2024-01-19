7a516f767f21096c0ef82b3c71266150b399ab24
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-01-19 17:03:08 +0900

    [Gold IV] Title: 오큰수, Time: 992 ms, Memory: 203456 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,41 +1,36 @@
-
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
-		int n = Integer.parseInt(br.readLine());
-		int[] a = new int[n];
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		int[] NGE = new int[N];
+		Stack<Integer> stack = new Stack<>();
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && A[stack.peek()] < A[i])
+				NGE[stack.pop()] = A[i];
+
+			stack.push(i);
+		}
+
+		while (!stack.isEmpty())
+			NGE[stack.pop()] = -1;
+
+		for (int n : NGE) {
+			sb.append(n).append(" ");
 		}
 		
-		Stack<Integer> s = new Stack<>();
-		int[] ans = new int[n];
-		for (int i = n - 1; i >= 0; i--) {
-			while (!s.isEmpty() && s.peek() <= a[i]) {
-				s.pop();
-			}
-			if (s.isEmpty()) {
-				ans[i] = -1;
-			} else {
-				ans[i] = s.peek();
-			}
-			s.push(a[i]);
-		}
-		for (int k : ans) {
-			sb.append(k + " ");
-		}
-		
-		System.out.print(sb.toString());
+		System.out.println(sb.toString());
 	}
 }
