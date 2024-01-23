2efe0f32a378ef41e7b9e1801a547385f9562eca
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-01-23 17:54:35 +0900

    [level 1] Title: K번째수, Time: 0.56 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int n = 0; n < commands.length; n++){
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
+            
+            int length = j - i + 1;
+            int[] subArray = new int[length];
+            for(int a = 0; a < length; a++){
+                subArray[a] = array[a + i - 1];
+            }
+            
+            Arrays.sort(subArray);
+            
+            answer[n] = subArray[k - 1];
+        }
+        return answer;
+    }
+}
