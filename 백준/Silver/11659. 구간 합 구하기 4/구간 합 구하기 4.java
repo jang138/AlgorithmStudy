2a8552b2ef55997fec8005db9024ff95ab9742ea
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-02-02 16:19:17 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 556 ms, Memory: 56188 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,28 +4,30 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String args[]) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int[] S = new int[N + 1];
 
+		int[] S = new int[N + 1];
 		st = new StringTokenizer(br.readLine());
+
 		for (int i = 1; i <= N; i++) {
-			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+			S[i] = Integer.parseInt(st.nextToken()) + S[i - 1];
 		}
 
-		for (int t = 0; t < M; t++) {
+		StringBuilder sb = new StringBuilder();
+		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
 			int i = Integer.parseInt(st.nextToken());
 			int j = Integer.parseInt(st.nextToken());
 
-			sb.append(S[j] - S[i - 1]).append("\n");
+			sb.append(S[j] - S[i-1]).append("\n");
 		}
-
+		
 		System.out.println(sb);
 	}
 
