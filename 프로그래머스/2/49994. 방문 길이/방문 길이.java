a2f3e06dfd3ccab3b1ca166eb7c11e8acd7d7914
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-04-02 21:35:01 +0900

    [level 2] Title: 방문 길이, Time: 16.62 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    public int solution(String dirs) {
+int[][] map = new int[11][11];
+		int x = 5;
+		int y = 5;
+		
+		HashSet<String> set = new HashSet<>();
+
+		for (int i = 0; i < dirs.length(); i++) {
+			char command = dirs.charAt(i);
+
+			int nx = x;
+			int ny = y;
+
+			switch (command) {
+			case 'U':
+				ny += 1;
+				break;
+
+			case 'D':
+				ny -= 1;
+				break;
+
+			case 'R':
+				nx += 1;
+				break;
+
+			case 'L':
+				nx -= 1;
+				break;
+			}
+
+			if (nx < 0 || nx >= 11 || ny < 0 || ny >= 11)
+				continue;
+				
+			set.add(String.format("(%d, %d) -> (%d, %d)", x, y, nx, ny));
+			set.add(String.format("(%d, %d) -> (%d, %d)", nx, ny, x, y));
+			
+//			System.out.println(String.format("(%d, %d) -> (%d, %d)", x, y, nx, ny));
+//			System.out.println(String.format("(%d, %d) -> (%d, %d)", nx, ny, x, y));
+
+			x = nx;
+			y = ny;
+		}
+		
+		return set.size() / 2;
+	}
+}
