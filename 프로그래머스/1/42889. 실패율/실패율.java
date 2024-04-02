ba1dd412f80bc997134a554eedc4ad9cb6cb7ad8
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-04-02 20:18:36 +0900

    [level 1] Title: 실패율, Time: 0.94 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,28 +1,35 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-		int[] challenger = new int[N + 2];
-        for (int i = 0; i < stages.length; i++) {
-            challenger[stages[i]] += 1;
-        }
+		int[] stageCount = new int[N + 1];
 
-
-		HashMap<Integer, Double> fails = new HashMap<>();
-        double total = stages.length;
-
-		for (int i = 1; i <= N; i++) {
-			if (challenger[i] == 0) {
-				fails.put(i, 0.);
-                continue;
-			}
-
-			fails.put(i, challenger[i] / total);
-			total -= challenger[i];
+		for (int i = 0; i < stages.length; i++) {
+			stageCount[stages[i]-1]++;
 		}
 
-		System.out.println(fails);
+		System.out.println(Arrays.toString(stageCount));
 
-		return fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue()))
-				.mapToInt(HashMap.Entry::getKey).toArray();
+		HashMap<Integer, Double> map = new HashMap<>();
+		double total = stages.length;
+
+		for (int i = 0; i < N; i++) {
+			if (stageCount[i] == 0) {
+				map.put(i+1, 0.0);
+				continue;
+			}
+
+			map.put(i+1, stageCount[i] / total);
+			total -= stageCount[i];
+		}
+
+		List<HashMap.Entry<Integer, Double>> entryList = new ArrayList<>(map.entrySet());
+		Collections.sort(entryList, (o1, o2) -> Double.compare(o2.getValue(), o1.getValue()));
+		
+		int answer[] = new int[entryList.size()];
+		for (int i = 0; i < entryList.size(); i++) {
+			answer[i] = entryList.get(i).getKey();
+		}
+        
+        return answer;
     }
 }
