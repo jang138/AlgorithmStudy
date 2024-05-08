b1b06ff3deee2509bf737b9dcdeaf5ed98a07510
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-05-08 16:26:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 1652 ms, Memory: 179120 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,42 +1,51 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		List<Node> list = new ArrayList<Node>();
+	static class Person {
+		int age;
+		String name;
+		int index;
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-            list.add(new Node(Integer.parseInt(st.nextToken()), st.nextToken()));
+		public Person(int age, String name, int index) {
+			this.age = age;
+			this.name = name;
+			this.index = index;
 		}
 
-        Collections.sort(list, (o1, o2) -> o1.age - o2.age );
-
-        for (Node node : list) {
-			System.out.println( node.age + " " + node.name );
+		@Override
+		public String toString() {
+			return String.format("%d %s", this.age, this.name);
 		}
-		
 	}
 
-	 public static class Node {
-	        int age;
-	        String name;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-	        public Node(int age, String name) {
-	            this.age = age;
-	            this.name = name;
-	        }
-	    }
+		PriorityQueue<Person> pq = new PriorityQueue<>((p1, p2) -> {
+			if (p1.age == p2.age)
+				return Integer.compare(p1.index, p2.index);
+			return Integer.compare(p1.age, p2.age);
+		});
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			Person p = new Person(Integer.parseInt(st.nextToken()), st.nextToken(), i);
+			pq.add(p);
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		while (!pq.isEmpty()) {
+			sb.append(pq.poll()).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
 }
