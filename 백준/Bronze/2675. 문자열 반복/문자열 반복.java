e4a4a979f0a6c8e8c4dff39b27fc8b7f4886abe4
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-05-21 13:12:56 +0900

    [Bronze II] Title: 문자열 반복, Time: 124 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -4,29 +4,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-
-		int T = Integer.parseInt(br.readLine());
-
+		
 		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int R = Integer.parseInt(st.nextToken());
 			String S = st.nextToken();
-
-			for (int i = 0; i < S.length(); i++) {
+			int length = S.length();
+			
+			for (int i = 0; i < length; i++) {
+				char c = S.charAt(i);
+				
 				for (int j = 0; j < R; j++) {
-					sb.append(S.charAt(i));
+					sb.append(c);
 				}
 			}
-
-			sb.append("\n");
+			
+			sb.append('\n');
 		}
 		
-		System.out.println(sb);
+		System.out.println(sb.toString());
 
 	}
-
 }
