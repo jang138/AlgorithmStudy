75974825cd18a42356be72eb93aab5a933516ee8
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-06-25 19:09:46 +0900

    [Silver III] Title: 소수 구하기, Time: 216 ms, Memory: 21184 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+
+		boolean[] prime = new boolean[N + 1];
+		prime[0] = true;
+		prime[1] = true;
+
+		for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+			if (prime[i])
+				continue;
+
+			for (int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = M; i < N + 1; i++) {
+			if (prime[i])
+				continue;
+
+			sb.append(i).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+
+}
