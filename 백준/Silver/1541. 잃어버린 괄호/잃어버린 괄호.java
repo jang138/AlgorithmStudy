fa242e40869ab3615832a890aa26ed59c492e54e
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-06-25 18:40:22 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,33 +3,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-		
-		// 음수를 기준으로 문자열을 나눠준 다음, 나눈 문자열을 각각 연산한 다음 전체에서 뺴준다 (단, 나눈 문자열 중 맨 처음은 양수이다)
+
 		int sum = 0;
-		int cnt = 0; // 첫번째 연산인지 판별용 (cnt가 0이면 첫번째)
-		
-		while(st.hasMoreTokens()) {
+		int cnt = 0;
+
+		while (st.hasMoreTokens()) {
 			int temp = 0;
-			
-			StringTokenizer plus = new StringTokenizer(st.nextToken(), "+");
-			
-			while(plus.hasMoreTokens()) {
-				temp += Integer.parseInt(plus.nextToken());
+
+			StringTokenizer bracket = new StringTokenizer(st.nextToken(), "+");
+			while (bracket.hasMoreTokens()) {
+				temp += Integer.parseInt(bracket.nextToken());
 			}
-			
-			if(cnt == 0) {
-				sum += temp;
-			} else {
+
+			if (cnt > 0)
 				sum -= temp;
-			}
-			
+			else
+				sum += temp;
+
 			cnt++;
 		}
-		
+
 		System.out.println(sum);
 	}
 
