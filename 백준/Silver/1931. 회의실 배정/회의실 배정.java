b61a12c028a9756c27904df0eea4213d81a681a0
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-06-28 20:28:17 +0900

    [Silver I] Title: 회의실 배정, Time: 584 ms, Memory: 46324 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,56 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
- 
-public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
 
-		int[][] time = new int[N][2];
-		
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		for(int i = 0; i < N; i++) {
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] time = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			time[i][0] = Integer.parseInt(st.nextToken());	
-			time[i][1] = Integer.parseInt(st.nextToken());	// 
+			time[i][0] = Integer.parseInt(st.nextToken());
+			time[i][1] = Integer.parseInt(st.nextToken());
 		}
-		
-		
-		Arrays.sort(time, new Comparator<int[]>() {
-			
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				
-				if(o1[1] == o2[1]) {
-					return o1[0] - o2[0];
-				}
-				
-				return o1[1] - o2[1];
-			}
- 
+
+		Arrays.sort(time, (o1, o2) -> {
+			if (o1[1] == o2[1])
+				return Integer.compare(o1[0], o2[0]);
+			return Integer.compare(o1[1], o2[1]);
 		});
-		
+
 		int count = 0;
-		int prev_end_time = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
-			if(prev_end_time <= time[i][0]) {
-				prev_end_time = time[i][1];
+		int end = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (end <= time[i][0]) {
+				end = time[i][1];
 				count++;
 			}
 		}
 		
 		System.out.println(count);
 	}
- 
+
 }
