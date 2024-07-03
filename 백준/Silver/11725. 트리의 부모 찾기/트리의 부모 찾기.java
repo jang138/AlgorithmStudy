12f5860dc9ac1061e55baf3c368c01b3828b548b
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-03 20:09:41 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 516 ms, Memory: 68952 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -7,51 +7,47 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static boolean[] visit;
-	static int[] parentNode;
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
-		N = Integer.parseInt(br.readLine());
-		visit = new boolean[N + 1];
-		parentNode = new int[N + 1];
+		int N = Integer.parseInt(br.readLine());
+		int[] root = new int[N + 1];
+
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
 		}
 
 		for (int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
-			graph.get(x).add(y);
-			graph.get(y).add(x);
+			graph.get(u).add(v);
+			graph.get(v).add(u);
 		}
+		
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		root[1] = 1;
 
-		dfs(1);
+		while (!queue.isEmpty()) {
+			int target = queue.poll();
 
-		for (int i = 2; i <= N; i++) {
-			sb.append(parentNode[i]).append("\n");
-		}
-
-		System.out.println(sb.toString());
-	}
-
-	private static void dfs(int n) {
-		visit[n] = true;
-
-		for (int node : graph.get(n)) {
-			if (!visit[node]) {
-				visit[node] = true;
-				parentNode[node] = n;
-				dfs(node);
+			for (int node : graph.get(target)) {
+				if (root[node] == 0) {
+					queue.add(node);
+					root[node] = target;
+				}
 			}
 		}
-	}
 
+		StringBuilder sb = new StringBuilder();
+		for (int i = 2; i < root.length; i++) {
+			sb.append(root[i]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
 }
