8001cd8e79a2cf6d2c68d41d7b86d0d2eb61eb2e
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-04 13:40:56 +0900

    [Silver II] Title: 연속합, Time: 200 ms, Memory: 22024 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,26 +4,25 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int seq[] = new int[N];
-		int dp[] = new int[N];
-		
+		int[] input = new int[N + 1];
+		int[] dp = new int[N + 1];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+			dp[i] = Math.max(dp[i - 1] + input[i], input[i]);
 		}
 		
-		dp[0] = seq[0];
-		int ans = dp[0];
-		
-		for (int i = 1; i < N; i++) {
-			dp[i] = Math.max(dp[i-1] + seq[i], seq[i]);
-			ans = Math.max(ans, dp[i]);
+		dp[1] = input[1];
+		int max = input[1];
+		for (int i = 1; i < dp.length; i++) {
+			max = Math.max(max, dp[i]);
 		}
 		
-		System.out.println(ans);
+		System.out.println(max);
 	}
 
 }
