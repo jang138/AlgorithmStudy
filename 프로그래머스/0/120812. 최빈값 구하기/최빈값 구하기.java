96d40fe82712c58209c3d07dba9518ffbe608538
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-11 13:50:38 +0900

    [level 0] Title: 최빈값 구하기, Time: 12.46 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -3,8 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] array) {
         ArrayList<Node> arrayList = new ArrayList<>();
-        arrayList.add(new Node(0, 0));
-		for (int i = 1; i <= 1000; i++) {
+		for (int i = 0; i <= 1000; i++) {
 			arrayList.add(new Node(i, 0));
 		}
 		
@@ -15,6 +14,8 @@ class Solution {
 		Collections.sort(arrayList, (o1, o2) -> {
 			return Integer.compare(o2.value, o1.value);
 		});
+        
+        System.out.println(arrayList);
 
 		return arrayList.get(0).value == arrayList.get(1).value ? -1 : arrayList.get(0).index;  
     }
@@ -27,5 +28,9 @@ class Solution {
 			this.index = index;
 			this.value = value;
 		}
+        
+        public String toString(){
+            return index + " " + value;
+        }
 	}
 }
