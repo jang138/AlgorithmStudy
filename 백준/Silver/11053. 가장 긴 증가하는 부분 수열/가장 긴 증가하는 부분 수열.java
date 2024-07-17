4d508157cf912929121183edc821570198eb73e7
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-17 18:42:36 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,14 +7,13 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int seq[] = new int[N + 1];
-		int dp[] = new int[N + 1];
+		int[] dp = new int[N + 1];
+		int[] seq = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			seq[i] = Integer.parseInt(st.nextToken());
 		}
-
 
 		int ans = Integer.MIN_VALUE;
 		for (int i = 1; i <= N; i++) {
