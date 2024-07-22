90fd7dd5ec66715e8af38f73c74f50ab8762b174
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-22 16:19:40 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,61 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	final static int Red = 0;
-	final static int Green = 1;
-	final static int Blue = 2;
-	
-	static int[][] Cost;
-	static int[][] DP;
-	
-	public static void main(String[] args) throws IOException {
-		
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		Cost = new int[N][3];
-		DP = new int[N][3];
-		
 		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			Cost[i][Red] = Integer.parseInt(st.nextToken());
-			Cost[i][Green] = Integer.parseInt(st.nextToken());
-			Cost[i][Blue] = Integer.parseInt(st.nextToken());
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] dp = new int[N + 1][3];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + Integer.parseInt(st.nextToken());
+			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + Integer.parseInt(st.nextToken());
+			dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + Integer.parseInt(st.nextToken());
 		}
-		
-		DP[0][Red] = Cost[0][Red];
-		DP[0][Green] = Cost[0][Green];
-		DP[0][Blue] = Cost[0][Blue];
-		
-		
-		System.out.println(Math.min(Paint_cost(N- 1, Red), Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue))));
+
+		System.out.println(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
 	}
-	
-	static int Paint_cost(int N, int color) {
-		
-		// 만약 탐색하지 않은 배열이라면
-		if(DP[N][color] == 0) {
-			
-			// color의 색에 따라 이전 집의 서로 다른 색을 재귀호출하여 최솟값과 현재 집의 비용을 더해서 DP에 저장한다
-			if(color == Red) {
-				DP[N][Red] = Math.min(Paint_cost(N - 1, Green), Paint_cost(N - 1, Blue)) + Cost[N][Red];
-			}
-			else if(color == Green) {
-				DP[N][Green] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Blue)) + Cost[N][Green];
-			}
-			else {
-				DP[N][Blue] = Math.min(Paint_cost(N - 1, Red), Paint_cost(N - 1, Green)) + Cost[N][Blue];
-			}
-			
-		}
-		
-		return DP[N][color];
-	}
+
 }
