6a76273d9657a142f106be531dd6f1b98a4e8cd4
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-07-22 16:29:16 +0900

    [Silver II] Title: 쇠막대기, Time: 240 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		ArrayDeque<Character> stack = new ArrayDeque<>();
+		int bar = 0;
+		int cnt = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+
+			if (c == '(')
+				stack.push(c);
+			else {
+				stack.pop();
+
+				if (str.charAt(i - 1) == '(')
+					cnt += stack.size();
+				else if (str.charAt(i - 1) == ')')
+					cnt += 1;
+			}
+		}
+
+		System.out.println(cnt);
+
+	}
+
+}
