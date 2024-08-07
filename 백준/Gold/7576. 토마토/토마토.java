541dc6505fe4c38197b0a994fa6eaf2a19d7d5f3
Jang WonSeok <58000549+jang138@users.noreply.github.com>
2024-08-07 23:37:24 +0900

    [Gold V] Title: 토마토, Time: 496 ms, Memory: 100544 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,42 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int M, N, max;
-	static int box[][];
-	static Queue<int[]> queue = new LinkedList<>();
-	static int dx[] = { 1, 0, -1, 0 };
-	static int dy[] = { 0, 1, 0, -1 };
+	static class Tomato {
+		int x, y, day;
+
+		public Tomato(int x, int y, int day) {
+			this.x = x;
+			this.y = y;
+			this.day = day;
+		}
+
+		@Override
+		public String toString() {
+			return x + " " + y + " " + day;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		box = new int[N][M];
-
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int[][] box = new int[N][M];
+		int[] dx = { -1, 0, 1, 0 };
+		int[] dy = { 0, -1, 0, 1 };
+		
+		ArrayDeque<Tomato> queue = new ArrayDeque<>();
+		int max = 0;
+		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				int a = Integer.parseInt(st.nextToken());
-				box[i][j] = a;
-
-				if (a == 1) {
-					queue.offer(new int[] { i, j, 0 }); // row, column, day
+				box[i][j] = Integer.parseInt(st.nextToken());
+				if (box[i][j] == 1) {
+					queue.add(new Tomato(i, j, 0));
 				}
 			}
 		}
 
-		bfs();
-		
-		Flag:
-		for (int i = 0; i < N; i++) {
+		while (!queue.isEmpty()) {
+			Tomato tomato = queue.poll();
+			max = Math.max(max, tomato.day);
+			
+//			System.out.println(tomato.toString());
+			
+			for (int d = 0; d < 4; d++) {
+				int nx = tomato.x + dx[d];
+				int ny = tomato.y + dy[d];
+				
+				if(nx < 0 || ny < 0 || nx >= N || ny >= M)
+					continue;
+				
+				if(box[nx][ny] != 0)
+					continue;
+				
+				queue.add(new Tomato(nx, ny, tomato.day + 1));
+				box[nx][ny] = 1;
+			}
+		}
+
+		Flag: for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if(box[i][j] == 0) {
 					max = -1;
@@ -46,31 +75,6 @@ public class Main {
 		}
 
 		System.out.println(max);
-	}
-
-	private static void bfs() {
-		while (!queue.isEmpty()) {
-			int[] now = queue.poll();
-
-//			System.out.println(Arrays.toString(now));
-
-			max = Math.max(max, now[2]);
-
-			for (int d = 0; d < 4; d++) {
-				int nx = now[0] + dx[d];
-				int ny = now[1] + dy[d];
-
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
-					continue;
-				}
-
-				if (box[nx][ny] == 0) {
-					queue.offer(new int[] { nx, ny, now[2] + 1 });
-					box[nx][ny] = 1;
-				}
-
-			}
-		}
 	}
 
 }
